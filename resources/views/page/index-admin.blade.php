76696b46c513be7b389983dccbd0808623916036
vlad.pliska <pluska2015@gmail.com>
2020-01-15 10:03:07 +0300

    nigth edit

---
+++ resources/views/page/index-admin.blade.php
@@ -0,0 +1,20 @@
+@include('./includes/head')
+<div class="container">
+@include('./layout/header')
+<div class="content-container">
+   <div class="admin">
+       <div class="admin-nav">
+        <a href="" class="admin-addBook btn-nav">Додати книжку</a>
+        <a href="" class="admin-stats btn-nav">Статистика</a>
+        <a href="" class="admin-allBook btn-nav">Книжки</a>
+        <a href="" class="admin-users btn-nav">Користувачі</a>
+        <a href="" class="admin-comments btn-nav">Коментарі</a>
+       </div>
+       <div class="admin-workspace">
+           
+       </div>
+    </div>
+
+</div>
+
+@include('.includes/footer') 
