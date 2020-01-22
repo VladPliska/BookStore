ec5e0cdcb25034e44781da61ffe550be6dc43c3c
vlad.pliska <pluska2015@gmail.com>
2020-01-22 10:08:19 +0300

    niigth changes

---
+++ resources/views/page/user.blade.php
@@ -0,0 +1,14 @@
+@include('./includes/head')
+<div class="container">
+@include('./layout/header')
+<div class="content-container">
+    <div class="user-page">
+        <div class="all-order">
+            <div class="title-order">Всі замовлення</div>
+        </div>
+        <div class="user-main-info"></div>
+        <div class="user-coments"></div>
+    </div>
+</div>
+
+@include('.includes/footer') 
