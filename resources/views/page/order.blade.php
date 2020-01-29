f15d76f5c097350c42064530c31efdae60e24656
vlad.pliska <pluska2015@gmail.com>
2020-01-29 09:11:07 +0200

    nigth changes

--- resources/views/page/order.blade.php
+++ resources/views/page/order.blade.php
@@ -4,12 +4,36 @@
 <div class="content-container">
 
 
-<div class="select-post">
-    <div class="title-select-post">Виберіть спосіб доставки</div>
-    <div class="new-post post">Нова пошта</div> 
-    <div class="uckr-post post">Укрпошта</div> 
-    <div class="mistExpress post">Містекспресс</div> 
-</div>
+<div class="buy-page">
+
+    <div class="select-post">
+        <span class="title-select-post title">Виберіть спосіб доставки</span>
+        <form id="select-post" action="#">
+            <div>
+                <input type="radio" name="post" id="new-post" class="new-post post"><label for="new-post">Нова пошта (50грн)</label>
+            </div>
+            <div>
+                <input type="radio" name="post" id="ukr-post" class="ukr-post post"><label for="ukr-post">Укрпошта (45грн)</label>
+            </div>
+            <div>
+                <input type="radio" name="post" id="mist" class="mistExpress post"><label for="mist">Містекспресс (45грн)</label> 
+            </div>
+            <input type="submit" style="display:none">
+        </form>
+    </div>
+
+    <div class="info-client">
+        <p>Контактні дані</p>
+        <input type="text" name="name" placeholder="Ім'я"><br>
+        <input type="text" name="surname" placeholder="Прізвище"><br>
+        <input type="text" name="number" placeholder="Номер телефону"><br>
+        <input type="text" name="email" placeholder="Email"><br>
+
+
+    </div>
+
+</div>  
+
 
 
 </div>
