103c6bb6a6639ade2cb96aece793706219ce3bf0
vlad.pliska <vlad@ceek.com>
2020-06-01 00:14:48 +0300

    debug

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -167,6 +167,7 @@ class AdminController extends Controller
                 'news' => true
             ]);
         } catch (\Exception $e) {
+            dd($e);
             return response()->json([
                 'news' => false
             ]);
